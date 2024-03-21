f729036f28bea9e6eef13d4d726acae4bf7ff226
Yinebeb-01 <yintar5@gmail.com>
2024-03-21 10:52:37 +0300

    transfer to bank api added

--- chapa.go
+++ chapa.go
@@ -13,13 +13,15 @@ import (
 )
 
 const (
-	acceptPaymentV1APIURL = "https://api.chapa.co/v1/transaction/initialize"
-	verifyPaymentV1APIURL = "https://api.chapa.co/v1/transaction/verify/%v"
+	acceptPaymentV1APIURL  = "https://api.chapa.co/v1/transaction/initialize"
+	verifyPaymentV1APIURL  = "https://api.chapa.co/v1/transaction/verify/%v"
+	transferToBankV1APIURL = "https://api.chapa.co/v1/transfers"
 )
 
 type API interface {
 	PaymentRequest(request *PaymentRequest) (*PaymentResponse, error)
 	Verify(txnRef string) (*VerifyResponse, error)
+	TransferToBank(request *BankTransfer) (*BankTransferResponse, error)
 }
 
 type chap struct {
@@ -110,4 +112,50 @@ func (c *chap) Verify(txnRef string) (*VerifyResponse, error) {
 	}
 
 	return &verifyResponse, nil
+}
+
+func (c *chap) TransferToBank(request *BankTransfer) (*BankTransferResponse, error) {
+	var err error
+	if err = request.Validate(); err != nil {
+		err := fmt.Errorf("invalid input %v", err)
+		log.Printf("error %v input %v", err, request)
+		return nil, err
+	}
+
+	data, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, transferToBankV1APIURL, bytes.NewBuffer(data))
+	if err != nil {
+		log.Printf("error %v", err)
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c.apiKey)
+	req.Close = true
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		log.Printf("error %v", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("error while reading resposne body %v", err)
+		return nil, err
+	}
+
+	response := BankTransferResponse{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Printf("error while unmarshaling  response %v", err)
+		return nil, err
+	}
+	return &response, nil
 }
